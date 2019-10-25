ee7eddcfa5aff2f1fb66cd1f094754ab3fa2a878
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-10-25 16:26:44 +0200

    Commit zéro

---
+++ cars-service/src/test/java/com/carsservice/CarsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ rentals-service/src/test/java/com/rentalsservice/RentalsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
