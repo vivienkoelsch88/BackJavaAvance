8a2ed265da9c915d7cc487adcdca2152f420b9bf
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-10-28 13:32:46 +0100

    Created Model, routes and controller for rentals crud

---
+++ rentals-service/src/main/java/com/rentalsservice/controllers/RentalsController.java
@@ -0,0 +1,46 @@
+package com.rentalsservice.controllers;
+
+import com.rentalsservice.models.Rental;
+import com.rentalsservice.repositories.RentalsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/rentals")
+@RestController
+public class RentalsController {
+
+    @Autowired
+    private RentalsRepository rentalsRepository;
+
+    @GetMapping("/")
+    public List<Rental> listRentals() {
+
+        return rentalsRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Rental findRental(@PathVariable int id) {
+
+        return rentalsRepository.findById(id);
+    }
+
+    @PostMapping("/create")
+    public void createRental(@RequestBody Rental rental) {
+
+        rentalsRepository.save(rental);
+    }
+
+    @PutMapping("/update")
+    public void updateRental(@RequestBody Rental rental) {
+
+        rentalsRepository.save(rental);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteRental(@PathVariable int id) {
+
+        rentalsRepository.deleteById(id);
+    }
+}
