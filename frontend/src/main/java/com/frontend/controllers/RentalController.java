ad2e837261090b1d38421aacb608a5320bf2f25a
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-10-30 11:27:23 +0100

    Added the possibility to create rental from backoffice

--- frontend/src/main/java/com/frontend/controllers/RentalController.java
+++ frontend/src/main/java/com/frontend/controllers/RentalController.java
@@ -1,5 +1,6 @@
 package com.frontend.controllers;
 
+import com.frontend.forms.RentalForm;
 import com.frontend.models.Rental;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -24,10 +27,43 @@ public class RentalController {
     public String listRentals(Model model) {
 
         List<Rental> rentals = restTemplate.getForObject(rentalsUrl + "/", List.class);
-        System.out.println(rentals);
 
         model.addAttribute("rentals", rentals);
 
         return "admin/rentalsList";
+    }
+
+    @GetMapping("/admin/createRental")
+    public String addRental(Model model) {
+
+        RentalForm rentalForm = new RentalForm();
+        model.addAttribute("rentalForm", rentalForm);
+
+        return "admin/createRental";
+    }
+
+    @PostMapping("/admin/createRental")
+    public String saveRental(Model model,
+                             @ModelAttribute("rentalForm") RentalForm rentalForm) {
+
+        System.out.println(rentalForm.getStartDate());
+        System.out.println(rentalForm.getStartDate().getClass());
+
+        int customerId = rentalForm.getCustomerId();
+        int carId = rentalForm.getCarId();
+        Date startDate = rentalForm.getStartDate();
+        Date endDate = rentalForm.getEndDate();
+
+        if (customerId > 0 &&
+            carId > 0 &&
+            startDate.getTime() > new Date().getTime() &&
+            endDate.getTime() > startDate.getTime()) {
+
+            restTemplate.postForObject(rentalsUrl + "/create", new Rental(customerId, carId, startDate, endDate), Rental.class);
+
+            return "redirect:/admin/rentals";
+        }
+
+        return "admin/createRental";
     }
 }
